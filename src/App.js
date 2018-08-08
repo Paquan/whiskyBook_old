434fae4df5329e75c70842f5eb6ebd01458e1967
Andy Bestvater <andy.bestvater@gmx.de>
2018-08-08 19:39:00 +0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <h1>Hello Whisky</h1>
+    );
+  }
+}
+
+export default App;
---
+++ src/tasting-notes/Tasting-Notes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TastingNotes extends React.Component {
+    render() {
+        return (
+            <h2>Tasting Notes</h2>
+        );
+    }
+}
