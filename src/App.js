749761c64c92387cf124d242ec16d63cdee36d11
Andy Bestvater <andy.bestvater@gmx.de>
2018-08-08 23:09:08 +0200

    Implement router and test components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, NavLink, Link} from "react-router-dom";
+import {TastingNotes} from './tasting-notes/TastingNotes'
+import {Home} from './home/Home'
 
-
-class App extends React.Component {
+export class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello Whisky</h1>
+      <Router>
+        <React.Fragment>
+          <Link to="/"><h1>Hello Whisky</h1></Link>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/tasting-notes">Tasting Notes</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" component={Home} />
+          <Route path="/tasting-notes" component={TastingNotes} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
-
-export default App;
--- src/tasting-notes/TastingNotes.js
+++ src/tasting-notes/TastingNotes.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class TastingNotes extends React.Component {
+export class TastingNotes extends React.Component {
     render() {
         return (
             <h2>Tasting Notes</h2>
