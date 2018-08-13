ce7ee015c1ac2688e0be37c57b9f6a75b9ba0a1a
Andy Bestvater <andy.bestvater@gmx.de>
2018-08-13 13:48:19 +0200

    Center back to Menu item and add spacing

--- src/shared/Page.js
+++ src/shared/Page.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./Page.css";
 import { Link } from "react-router-dom";
 
 export class Page extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Link to="/">
-          <img src="http://via.placeholder.com/200x200" alt="back to menu" />
-        </Link>
+        <div className="backToMenu">
+          <Link to="/">
+            <img src="http://via.placeholder.com/200x200" alt="back to menu" />
+          </Link>
+        </div>
         {this.props.children}
       </React.Fragment>
     );
